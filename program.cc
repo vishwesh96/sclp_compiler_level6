b26f4928619f942e10a334257b6e69673f5f3d28
vishwesh96 <vishwesh1696@gmail.com>
2017-03-31 17:03:42 +0530

    started writing program.cc

--- program.cc
+++ program.cc
@@ -14,65 +14,72 @@ Program program_object;
 
 Program::Program()
 {
-
+	//TODO6
 }
 
 Program::~Program()
 {
-
+	//TODO6
 }
 
 void Program::set_procedure(Procedure * proc, int line)
 {
-
+	procedure = proc;
+	//TODO6
 }
 
 void Program::print_sym()
 {
-
+	//TODO6
+	global_symbol_table.print();
+	procedure->print_sym();
 }
 
 void Program::print()
 {
 
+	//TODO6
+	procedure->print();
 }
 
 void Program::global_list_in_proc_check()
 {
 
+	//TODO6
 }
 
 void Program::set_global_table(Symbol_Table & new_global_table)
 {
-
+	global_symbol_table = new_global_table;
 }
 
 bool Program::variable_in_symbol_list_check(string variable)
 {
-
+	return global_symbol_table.variable_in_symbol_list_check(variable);
 }
 
 bool Program::variable_proc_name_check(string symbol)
 {
-
-}
+	return (procedure->get_proc_name()==symbol);
+}	
 
 Symbol_Table_Entry & Program::get_symbol_table_entry(string variable)
 {
-
+	return global_symbol_table.get_symbol_table_entry(variable)
 }
 
 void Program::compile()
 {
-
+	procedure->compile();
 }
 
 void Program::delete_all()
 {
-
+	delete(procedure);
+	global_symbol_table.~Symbol_Table();
 }
 
 bool Program::variable_in_proc_map_check(string variable)
 {
-
+	
 }
